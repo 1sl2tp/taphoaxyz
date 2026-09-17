804b51c051a583a81421b55635b7194097fd61ac
1sl2tp <1sl2tp@gmail.com>
2026-09-17 19:09:38 +0700

    test: target source delete modal z-order correctly

--- tests/fixed-ui-source-delete-modal.test.js
+++ tests/fixed-ui-source-delete-modal.test.js
@@ -5,8 +5,6 @@ import {readFile} from 'node:fs/promises';
 const markup=await readFile('src/fixed-ui-markup-5.js','utf8');
 
 test('source delete confirmation renders above source picker',()=>{
-  assert.match(markup,/id=\\"productEditorSourcePickerModal\\"/);
-  assert.match(markup,/productEditorSourcePickerModal[\s\S]*z-\[140\]/);
-  assert.match(markup,/id=\\"confirmModal\\"/);
-  assert.match(markup,/confirmModal[\s\S]*z-\[160\]/);
+  assert.match(markup,/z-\[140\][^\"]*\\" id=\\"productEditorSourcePickerModal\\"/);
+  assert.match(markup,/z-\[160\][^\"]*\\" id=\\"confirmModal\\"/);
 });
