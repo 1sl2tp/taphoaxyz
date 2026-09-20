e2f7b81a2dd17567cfd29f964c7323f63a0df2f5
1sl2tp <1sl2tp@gmail.com>
2026-09-20 22:36:03 +0700

    Rotate PWA cache for market pack rules

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v10';
+const CACHE_NAME='taphoa-runtime-v11';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
