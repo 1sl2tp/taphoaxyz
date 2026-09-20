9b31bcbddeae1437355a73a68e5b4b7b84a43e82
1sl2tp <1sl2tp@gmail.com>
2026-09-20 23:41:33 +0700

    Rotate PWA cache for source labels and market colors

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v22';
+const CACHE_NAME='taphoa-runtime-v23';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
