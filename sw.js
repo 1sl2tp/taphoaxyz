d8f1d99fb3f1b8319b6596c767b378be08fd2ccf
1sl2tp <1sl2tp@gmail.com>
2026-09-20 21:23:16 +0700

    Rotate PWA cache for rounded comparison

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v7';
+const CACHE_NAME='taphoa-runtime-v8';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
