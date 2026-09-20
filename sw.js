acaa7987459469a88b5b903a0cd9988219d2c1dd
1sl2tp <1sl2tp@gmail.com>
2026-09-20 22:53:37 +0700

    Rotate PWA cache for pack table and GO links

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v13';
+const CACHE_NAME='taphoa-runtime-v14';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
