e0113bf174b1983197894e9bf84ac67ac33e7696
1sl2tp <1sl2tp@gmail.com>
2026-09-20 22:14:32 +0700

    Rotate PWA cache for sales card market comparison

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v8';
+const CACHE_NAME='taphoa-runtime-v9';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
