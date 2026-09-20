3d4d54037deb49a9eebb70c05a52befc4e880be4
1sl2tp <1sl2tp@gmail.com>
2026-09-20 20:52:09 +0700

    Rotate PWA cache for shared comparison grid

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v3';
+const CACHE_NAME='taphoa-runtime-v4';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
