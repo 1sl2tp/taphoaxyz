78e75b4be44579bd3650ff15175fb4700e1fe130
1sl2tp <1sl2tp@gmail.com>
2026-09-21 00:19:51 +0700

    Update product image cache contract for infinite scroll

--- tests/fixed-ui-product-images.test.js
+++ tests/fixed-ui-product-images.test.js
@@ -161,7 +161,7 @@ test('product media comparison assets bypass stale PWA cache',async()=>{
   const [index,sw]=await Promise.all([read('index.html'),read('sw.js')]);
   assert.match(index,/fixed-ui-product-media\.css\?v=compare-fit-20260920/);
   assert.match(index,/fixed-ui-product-media\.js\?v=compare-fit-20260920/);
-  assert.match(sw,/taphoa-runtime-v27/);
+  assert.match(sw,/taphoa-runtime-v28/);
 });
 
 
