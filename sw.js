b1545817eac1958f3af57b48401fac4401e6bf75
1sl2tp <1sl2tp@gmail.com>
2026-09-20 23:09:43 +0700

    Rotate PWA cache for balanced compact product detail

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v16';
+const CACHE_NAME='taphoa-runtime-v17';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
