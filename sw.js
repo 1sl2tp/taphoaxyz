dab64cd4eee82cdf26712d41864ed5e90fc5f1bb
1sl2tp <1sl2tp@gmail.com>
2026-09-21 16:39:52 +0700

    chore: rotate PWA runtime cache to v34

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v33';
+const CACHE_NAME='taphoa-runtime-v34';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
