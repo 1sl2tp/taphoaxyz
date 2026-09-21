df9ffe6a5a85190aa6cf8763c7d0a338ba2401e2
1sl2tp <1sl2tp@gmail.com>
2026-09-21 16:17:16 +0700

    chore: rotate runtime cache for iPhone share fix

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const CACHE_PREFIX='taphoa-runtime-';
-const CACHE_NAME='taphoa-runtime-v31';
+const CACHE_NAME='taphoa-runtime-v32';
 
 self.addEventListener('install',event=>{
   event.waitUntil(self.skipWaiting());
